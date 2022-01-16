09f478f2edb024639d467733d9491d2e25d5f16b
Hendra Putu <hendra@slash.co>
2022-01-16 13:26:00 +0800

    06 styling react components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import AppExpense from './03-react-basics-working-with-components/AppExpense';
 import AppExpense2 from './04-react-state-events/AppExpense2';
 import AppExpense3 from './05-rendering-lists-conditional-content/AppExpense3';
+import AppStyling from './06-styling/AppStyling';
+
+import './App.css';
 
 function App() {
   return (
-    <AppExpense3 />
+    <>
+      <div className='lecture'>
+      <AppExpense />
+      </div>
+      <div className='lecture'>
+      <AppExpense2 />
+      </div>
+      <div className='lecture'>
+      <AppExpense3 />
+      </div>
+      <div className='lecture'>
+      <AppStyling />
+      </div>
+    </>
   );
 }
 
